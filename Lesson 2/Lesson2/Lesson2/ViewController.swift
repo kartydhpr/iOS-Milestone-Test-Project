fcfbc72c55de70ff6b5094ee78d75fcfe23b6147
Karteikay Dhuper <karteikaydhuper@Kartys-Laptop.local>
2022-02-08 18:37:08 -0500

    uploading lesson 2 folder

---
+++ Lesson 2/Lesson2/Lesson2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson2
+//
+//  Created by Karteikay Dhuper on 2/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
